df7c18693395032675badaaef382b96b8cc8a89f
kaznad <kazak21@gmail.com>
2023-06-02 16:03:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Cache Invalidation?
+          Edit <code>src/App.js</code> and save to reload. Email Notification + Lambda Trigger + Cache Invalidation
         </p>
         <a
           className="App-link"
